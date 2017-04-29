af733d3bc52487150726e3a17ee52d30d663c26b
Walunt Geek <wg@walnutgeek.com>
2017-04-28 23:23:53 -0700

    setup files - upload to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
 setup(name='hashstore',
-      version='0.1',
+      version='0.0.3',
       description='Content Addressable Storage',
       url='https://github.com/walnutgeek/hashstore',
       author='Walnut Geek',
       author_email='wg@walnutgeek.com',
       license='Apache 2.0',
-      packages=open('requirements.txt').read().split(),
+      packages=['hashstore'],
+      entry_points={
+          'console_scripts': [.
+              'shash=hashstore.shash:main',
+          ],
+      },
+      install_requires=open('requirements.txt').read().split(),
       zip_safe=False)
