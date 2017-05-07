21d228c95ad574c617b57bb4d239cd5f62a21f2d
Walunt Geek <wg@walnutgeek.com>
2017-05-06 22:47:15 -0700

    cleanup redundant backup logic and config

--- hashstore/tests/server_tests.py
+++ hashstore/tests/server_tests.py
@@ -13,32 +13,6 @@ log = test.log
 test_config = os.path.join(os.path.dirname(__file__), 'hashery.back-it-up')
 substitutions = {'{test_dir}': test.dir}
 
-import hashstore.backup
-
-def test_backup():
-    hashery_dir = os.path.join(test.dir, 'insecure')
-    os.makedirs(hashery_dir)
-    port = 9753
-    test.run_shash('start --insecure --store_dir %s --port %d' % (hashery_dir, port))
-    time.sleep(2)
-    files = os.path.join(test.dir, 'files')
-    prep_mount(files, file_set1)
-    b = hashstore.backup.Backup.from_config(test_config, os.path.join(test.dir,'backup.db'), substitutions)
-    v1 = b.backup()
-    prep_mount(files, file_set2)
-    v2 = b.backup()
-    eq_(str(v1[files]), fileset1_udk)
-    eq_(str(v2[files]), fileset2_udk)
-    f1 = os.path.join(test.dir, 'files1')
-    b.restore(v1[files], f1)
-    f2 = os.path.join(test.dir, 'files2')
-    b.restore(v2[files], f2)
-    eq_(str(mount.MountDB(f1).scan()[1]), fileset1_udk)
-    eq_(str(mount.MountDB(f2).scan()[1]), fileset2_udk)
-    test.run_shash_and_wait('stop --port %d' % port)
-
-    time.sleep(.5)
-
 
 def test_all_access_modes():
     def do_test(secure):
